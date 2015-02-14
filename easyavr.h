d39156a872e916e5e18ca119738a6e0a6298a1e1
MultiMote <sporeon@yandex.ua>
2015-02-14 19:54:25 +0300

    upd easyavr

--- easyavr.h
+++ easyavr.h
@@ -1,7 +1,8 @@
 #ifndef __EASYAVR_H_
 #define __EASYAVR_H_
 
-#define PIN_ON(x,p) ((PORT ## x) |= 1<<(p))
-#define PIN_OFF(x,p) ((PORT ## x) &= ~(1<<(p)))
+#define PIN_ON(x,p) ((PORT ## x) |= _BV(P ## x ## p))
+#define PIN_OFF(x,p) ((PORT ## x) &= ~_BV(P ## x ## p))
+#define PIN_SET(x,p,v) (v > 0 ? PIN_ON(x,p) : PIN_OFF(x,p))
 
 #endif
