a41861a8f48eee3d2d8d591f6d157f35d0c66be6
MultiMote <sporeon@yandex.ua>
2015-02-14 20:39:18 +0300

    upd easyavr

--- easyavr.h
+++ easyavr.h
@@ -1,8 +1,33 @@
 #ifndef __EASYAVR_H_
 #define __EASYAVR_H_
 
-#define PIN_ON(x,p) ((PORT ## x) |= _BV(P ## x ## p))
-#define PIN_OFF(x,p) ((PORT ## x) &= ~_BV(P ## x ## p))
-#define PIN_SET(x,p,v) (v > 0 ? PIN_ON(x,p) : PIN_OFF(x,p))
+
+// Sets pin of port to 1
+//
+//  Example for PD2: PIN_ON(D, 2)
+#define PIN_ON(port,pin) ((PORT ## port) |= _BV(P ## port ## pin))
+
+
+// Sets pin of port to 0
+//
+//  Example for PD2: PIN_OFF(D, 2)
+#define PIN_OFF(port,pin) ((PORT ## port) &= ~_BV(P ## port ## pin))
+
+
+// Sets pin of port to 0
+//
+//  Example for PD2: PIN_SET(D, 2, 1)
+#define PIN_SET(port,pin,val) (val > 0 ? PIN_ON(port,pin) : PIN_OFF(port,pin))
+
+
+// Sets all of port pins to OUTPUT mode
+//
+//  Example for PORTD: PORT_AS_OUTPUT(D)
+#define PORT_AS_OUTPUT(port) ((DDR ## port) = 0xFF)
+
+// Sets all of port pins to INPUT mode
+//
+//  Example for PORTD: PORT_AS_INPUT(D)
+#define PORT_AS_INPUT(port) ((DDR ## port) = 0x00)
 
 #endif
