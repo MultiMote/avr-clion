cfbf202391816bc4ebcc173892b3c2af7d2a4bd5
MultiMote <sporeon@yandex.ua>
2019-10-22 23:01:28 +0300

    Remove trash

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "easyavr.h"
 
 #define LED_PORT PORTD
 #define LED_PIN 0
@@ -8,12 +7,13 @@
 
 int main(void)
 {
-    PIN_AS_OUTPUT(LED_DDR, LED_PIN);
+    LED_DDR |= _BV(LED_PIN);
 
     while (1) {
-        PIN_ON(LED_PORT, LED_PIN);
+        LED_PORT |= _BV(LED_PIN);
         _delay_ms(100);
-        PIN_OFF(LED_PORT, LED_PIN);
+        
+        LED_PORT &= ~_BV(LED_PIN);
         _delay_ms(100);
     }
 }
